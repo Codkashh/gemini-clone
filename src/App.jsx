2dd9b88de40097a8db1a4e1b552f4f8647df3562
Codkashh <as3733542@gmail.com>
2024-09-23 15:57:51 +0530

    ui is updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Sidebar from "./components/sidebar/sidebar"
+import Main from "./components/main/main"
 
 function App() {
 
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   )
 }
